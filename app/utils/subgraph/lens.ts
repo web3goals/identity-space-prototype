ad668d7e953f6aeeff0e342ee10724e148d15afe
kiv1n <aloha@kiv1n.ru>
2023-10-27 10:37:47 +0300

    Use lens subgraph in the app

--- app/utils/subgraph/lens.ts
+++ app/utils/subgraph/lens.ts
@@ -1,13 +1,38 @@
 import { Identity } from "@/types/identity";
+import { makeSubgraphQuery } from "./helper";
 
-// TODO:
 export async function findAddressByLens(
   lens: string
 ): Promise<string | undefined> {
-  return "";
+  const query = `{
+    profiles(first: 1, where: {handle : "${lens.toLowerCase()}"}) {
+      owner
+    }
+  }`;
+  const result = await makeSubgraphQuery(
+    process.env.NEXT_PUBLIC_SUBGRAPH_LENS as string,
+    query
+  );
+  return result.profiles?.[0]?.owner;
 }
 
-// TODO:
 export async function findLensIdentities(address: string): Promise<Identity[]> {
-  return [];
+  const query = `{
+    profiles(first: 100, where: {owner : "${address.toLowerCase()}"}) {
+      handle
+    }
+  }`;
+  const result = await makeSubgraphQuery(
+    process.env.NEXT_PUBLIC_SUBGRAPH_LENS as string,
+    query
+  );
+  const identites: Identity[] = [];
+  for (const profile of result.profiles) {
+    identites.push({
+      address: address,
+      identity: profile.handle,
+      platform: "Lens",
+    });
+  }
+  return identites;
 }
